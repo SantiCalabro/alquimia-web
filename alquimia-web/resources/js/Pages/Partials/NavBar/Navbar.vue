31affa9ac23b0a29a58cc692b944570a5b03d437
SantiCalabro <santi.calabro93@gmail.com>
2023-11-20 19:19:27 -0300

    first commit

---
+++ alquimia-web/resources/js/Pages/Partials/NavBar/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div class="sub-container">
+            <img :src=logoSrc alt="Logo de Alquimia" class="logo">
+            <ul>
+                <li>Inicio</li>
+                <li>Nosotrxs</li>
+                <li>Trenzas</li>
+                <li>Piercing</li>
+                <li>Tattoo</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style src="./Navbar.css" scoped></style>
+<script>
+import logo from "../../../../assets/Alquimia_white.png"
+export default {
+    data() {
+        return {
+            logoSrc: logo,
+        }
+    },
+    name: 'Navbar',
+}
+</script>
